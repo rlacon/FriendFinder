a854b32921a2bc1361cd3339b3203b99ca547aeb
Ryan Lacon <ryan.lacon@gmail.com>
2019-08-06 12:37:07 -0400

    Photo now appears under friend match. Added node_modules to .gitignore. CSS styling will now appear on local server.

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('app/public'));
 
 
 // ================================================================================
