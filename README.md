11bbdbe34d507456f17ab19c9f46909de043d887
Ryan Lacon <ryan.lacon@gmail.com>
2019-08-06 22:47:09 -0400

    Added more friend matches to the object array.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FriendFinder
+
+FriendFinder is a friend-making application that matches users based on their compatibility. Their compatibility gets determined by a survey where the user answers ten questions on a 1-5 answer scale. Their total scores are added up, and the closest differences between other users are how their compatibility is determined.
--- app/data/friends.js
+++ app/data/friends.js
@@ -5,9 +5,19 @@
 
 var friendsArray = [
     {
-        name: "Ryan",
-        photo: "https://ryanlacon.codes/images/me.jpg",
-        scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+        name: "Captain America",
+        photo: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/d/d7/CapAmerica-EndgameProfile.jpg/revision/latest/scale-to-width-down/620?cb=20190423175339",
+        scores: ["5", "5", "5", "3", "5", "1", "2", "1", "5", "5"]
+    },
+    {
+        name: "Tony Stark",
+        photo: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/3/35/IronMan-EndgameProfile.jpg/revision/latest/scale-to-width-down/620?cb=20190423175213",
+        scores: ["5", "2", "5", "1", "5", "5", "5", "3", "1", "3"]
+    },
+    {
+        name: "Bruce Banner",
+        photo: "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/a/a4/ProfHulkAvengersEndgamelicensingart001.jpg/revision/latest?cb=20190506122658",
+        scores: ["5", "1", "1", "1", "2", "3", "3", "1", "4", "1"]
     }
 ];
 
